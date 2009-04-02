d849be0e77ca3b1efbd2a8d52cd260d43e1e82ac
Maik Gosenshuis <maik@gosenshuis.nl>
2009-04-02 12:54:30 +0200

    Moved the models. Added documentation to the code.

--- main.py
+++ main.py
@@ -11,28 +11,40 @@ class World(DirectObject):
         
         # Load the environment in which Ralph will walk. Set its parent
         # to the render variable so that it is a top-level node.
-        self.env = loader.loadModel('models/world.egg.pz')
+        self.env = loader.loadModel('models/world/world.egg.pz')
         self.env.reparentTo(render)
         self.env.setPos(0, 0, 0)
         
-        # Create an Actor instance for Ralph.
-        self.ralph = Actor('models/ralph.egg.pz', {
-            'run': 'models/ralph-run.egg.pz',
-            'walk': 'models/ralph-walk.egg.pz'
+        # Create an Actor instance for Ralph. We also specify the animation
+        # models that we want to use as a dictionary, where we can use to
+        # keys to refer to the animations later on. The start point of Ralph
+        # is hardcoded in the world model somewhere, so we look that up.
+        self.ralph = Actor('models/ralph/ralph.egg.pz', {
+            'run': 'models/ralph/ralph-run.egg.pz',
+            'walk': 'models/ralph/ralph-walk.egg.pz'
         })
         self.ralph.reparentTo(render)
         self.ralph.setPos(self.env.find('**/start_point').getPos())
         
+        # Create a floater object that always floats 2 units above Ralph.
+        # We make sure that it is attached to Ralph by reparenting it to
+        # Ralph's object instance.
         self.floater = NodePath(PandaNode('floater'))
         self.floater.reparentTo(self.ralph)
         self.floater.setZ(self.floater.getZ() + 2)
         
+        # Disable any mouse input, including moving the camera around with
+        # the mouse.
         base.disableMouse()
-        base.camera.setPos(self.ralph.getX() + 10, self.ralph.getY() + 10, 50)
         
+        # Set the initial position for the camera as X, Y and Z values.
+        base.camera.setPos(self.ralph.getX(), self.ralph.getY() + 10, 2)
+        
+        # Schedule the move method to be executed in the game's main loop.
         taskMgr.add(self.move, 'move')
     
     def move(self, task):
+        # Set the camera to look at the floater object above Ralph.
         base.camera.lookAt(self.floater)
 
 w = World()
